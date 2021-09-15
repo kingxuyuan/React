b35e7e9742ebd8e2b4fa22bd68a1924259299462
lemon <lemon@gmail.com>
2021-09-15 14:20:23 +0800

    first commit

--- src/store/rootSaga.ts
+++
@@ -1,9 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-
-import tokenSaga from './sagas/token';
-
-export default function* rootSaga () {
-    yield all([
-        fork(tokenSaga)
-    ])
-}
--- src/store/selectors/token.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from "reselect";
-import { AppState } from "../rootReducer";
-
-const setToken = (state: AppState) => state.tokenReducer.token;
-
-export const getToken = createSelector(setToken, (token) => token);
